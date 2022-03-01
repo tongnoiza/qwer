232866cebccb9f0fc6a596c3faa4ace493bcdbed
sontayatong <98791627+sontayatong@users.noreply.github.com>
2022-03-01 20:45:50 +0700

    aaa

---
+++ k.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    .input{
+        width: 25px;
+        height: 25px;
+        font-size: 45px;
+    }
+</style>
+<body>
+
+    <?php
+    
+    for ($i=0; $i < ; $i++) { 
+       for ($c=0; $c < ; $c++) { 
+         echo "<input type='button' value=''>";
+       }
+    }
+    
+    
+    ?>
+</body>
+</html>
